63e287aef576bed5b92287d3fcdda6fe8e607340
shouta-fujino <shouta.fujino@bizreach.co.jp>
2016-09-30 21:08:06 +0900

    unk

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AIzaSyD4SkV7n_OogiJsqz4yurNAwXDnMYPsoHs'
+};
--- sample_command.sh
+++
@@ -1,4 +0,0 @@
-curl --header "Authorization: key=AIzaSyAwXvwk0M3ivaaJNtwiQ9UE08y6UHTyBBk" \
-     --header Content-Type:"application/json" \
-     https://fcm.googleapis.com/fcm/send \
-     -d "{\"to\": \"/topics/news\",\"priority\":\"high\",\"notification\": {\"title\": \"this is title\", \"body\": \"this is body\", \"icon\": \"ic_stat_ic_notification\"}}"
