c0de4127f4de411ca45aa08fdbd72eb00cbf5305
syplus2 <syplus2@gmail.com>
2016-10-03 17:26:25 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,7 @@
 self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
 
+  // test
   var title = 'Yay a message.';
   var body = 'We have received a push message.';
   var icon = 'images/icon-192x192.png';
